585e774d19b1c5a00efa27fbd4354588811e1119
Lau Yun Xian <126446719+lauyunxian@users.noreply.github.com>
2024-10-06 06:37:39 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,10 @@
+var vg_1 = "countries_inc_lvl_map.vg.json";
+
+vegaEmbed("#map", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "avg_income.vg.json";
+
+vegaEmbed("#line", vg_2).then(function(result) {
+}).catch(console.error);
+
